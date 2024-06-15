b480c68f2b8269c3f25b060f800c0bb78e8141ab
Hasintha Dilshan <127721393+H4S1NTH4@users.noreply.github.com>
2024-06-16 04:22:48 +0530

    initial commit

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Hero.css'
+import Navbar from '../Navbar/Navbar'
+import profile_img from '../../assets/cropped prof pic.jpg'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <img src={profile_img} alt="" className="profile-img" />
+        <h1><span>I am Hasintha Dilshan,</span> SE Undergraduate at SLIIT. </h1>
+        <p>I am a software engineering undergraduate from SLIIT. I mainly focused o java and additionally I am
+            enthusiastic about IoT.
+        </p>
+        <div className="hero-action">
+            <div className='hero-connect'>Connect with me</div>
+            <div >//</div>
+            <div className='hero-resume'>My Resume</div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Hero
